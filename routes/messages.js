85e0d0ede27d7559fffe1a8b9a2e0cc97b7e3f1f
Dipesh B C <bcdipeshwork@gmail.com>
2023-11-03 21:51:32 -0400

    Fix messages route

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,11 @@
+/** Message route for message.ly */
+
+const express = require("express");
+const router = express.Router();
+const { authenticateJWT, ensureLoggedIn } = require("../middleware/auth");
+const Message = require("../models/message");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -7,10 +15,24 @@
  *               from_user: {username, first_name, last_name, phone},
  *               to_user: {username, first_name, last_name, phone}}
  *
- * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", authenticateJWT, ensureLoggedIn, async (req, res, next) => {
+  try {
+    const message = await Message.get(req.params.id);
 
+    if (
+      req.user.username === message.from_user.username ||
+      req.user.username === message.to_user.username
+    ) {
+      return res.json(message);
+    }
+
+    throw new ExpressError("Unauthorized.", 401);
+  } catch (err) {
+    next(err);
+  }
+});
 
 /** POST / - post message.
  *
@@ -18,13 +40,49 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", authenticateJWT, ensureLoggedIn, async (req, res, next) => {
+  try {
+    const { to_username, body } = req.body;
 
+    if (!to_username || !body) {
+      throw new ExpressError("Both Username and Password is required.", 400);
+    }
+
+    const message = await Message.create({
+      from_username: req.user.username,
+      to_username,
+      body,
+    });
+
+    return res.json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
 /** POST/:id/read - mark message as read:
  *
  *  => {message: {id, read_at}}
  *
- * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post(
+  "/:id/read",
+  authenticateJWT,
+  ensureLoggedIn,
+  async (req, res, next) => {
+    try {
+      const message = await Message.get(req.params.id);
 
+      if (message.to_user.username === req.user.username) {
+        await Message.markRead(req.params.id);
+
+        return res.status(200);
+      }
+
+      throw new ExpressError("Unauthorized.", 401);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,19 @@
 
 const express = require("express");
 const router = express.Router();
+const {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+} = require("../middleware/auth");
 const User = require("../models/user");
-const ExpressError = require("../expressError");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-router.get("/", async (req, res, next) => {
+router.get("/", authenticateJWT, ensureLoggedIn, async (req, res, next) => {
   try {
     const users = await User.all();
 
@@ -25,15 +29,21 @@ router.get("/", async (req, res, next) => {
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-router.get("/:username", async (req, res, next) => {
-  try {
-    const user = await User.get(req.params.username);
+router.get(
+  "/:username",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  async (req, res, next) => {
+    try {
+      const user = await User.get(req.params.username);
 
-    return res.json(user);
-  } catch (err) {
-    next(err);
+      return res.json(user);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 /** GET /:username/to - get messages to user
  *
@@ -44,15 +54,21 @@ router.get("/:username", async (req, res, next) => {
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/to", async (req, res, next) => {
-  try {
-    const messages = await User.messagesTo(req.params.username);
+router.get(
+  "/:username/to",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  async (req, res, next) => {
+    try {
+      const messages = await User.messagesTo(req.params.username);
 
-    return res.json(messages);
-  } catch (err) {
-    next(err);
+      return res.json(messages);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 /** GET /:username/from - get messages from user
  *
@@ -63,12 +79,18 @@ router.get("/:username/to", async (req, res, next) => {
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/from", async (req, res, next) => {
-  try {
-    const messages = await User.messagesFrom(req.params.username);
+router.get(
+  "/:username/from",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  async (req, res, next) => {
+    try {
+      const messages = await User.messagesFrom(req.params.username);
 
-    return res.json(messages);
-  } catch (err) {
-    next(err);
+      return res.json(messages);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
